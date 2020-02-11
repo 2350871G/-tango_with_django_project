ded529d56b18791c07a178edc1eebe8998d71270
Lewis Grimmer <2350871G@student.gla.ac.uk>
2020-02-11 10:13:05 +0000

    Chapter 10 complete

--- rango/views.py
+++ rango/views.py
@@ -14,6 +14,8 @@ from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 
 def index(request):
 
@@ -32,13 +34,25 @@ def index(request):
 
     context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html' , context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html' , context=context_dict)
+
+    return response
 
 def about(request):
+
+    visitor_cookie_handler(request)
+
+    context_dict = {}
+
+    if request.session['visits'] != None:
+        context_dict['visits'] = request.session['visits']
+
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -194,3 +208,29 @@ def user_logout(request):
     logout(request)
 
     return redirect(reverse('rango:index'))
+
+#Helper function to track visits to site with cookies
+def visitor_cookie_handler(request):
+
+    visits = int(get_server_side_cookie(request,'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now()-last_visit_time).days > 0:
+        visits+= 1
+
+        request.session['last_visit'] = str(datetime.now())
+
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] =  visits
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
